5e47126b09952c32b572d41459d2474ebce1d430
Hoyue <lysdnosed@gmail.com>
2024-11-19 11:11:04 +0800

    feat(server): Add CORS middleware and health check routes
    - Added CORS middleware to the server to support cross-domain requests.
    - Implemented a health check route to check the status of the server.
    - These changes improve the availability and maintainability of the server.

--- server/main.py
+++ server/main.py
@@ -1,8 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from server.api import ai, image
 
 app = FastAPI()
 
-# 注册路由
+origins = [
+    "*"
+    # Wait for other origins
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST", "GET"],
+    allow_headers=["*"],
+    expose_headers=["*"]
+)
+
 app.include_router(ai.router)
 app.include_router(image.router)
+
+@app.get("/status")
+async def health_check():
+    return {
+        "status": "ok",
+        "code": 200
+    }
